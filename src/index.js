788fd42471ca759cfd6ae77f280fb8dfa99bd788
abysmli <abysmli@gmail.com>
2018-06-15 16:10:09 +0200

    fix bugs

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class SecUtils {
   getDatetime (anyUnixtime) {
     let date = new Date(anyUnixtime)
     let Y = date.getFullYear() + '-'
-    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
     let D = date.getDate() + ' '
     let h = date.getHours() + ':'
     let m = date.getMinutes() + ':'
